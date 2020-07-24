8170d4b4313717ea170383725dd10ecefa825fb0
Hsuvas Borkakoty <31013601+hsuvas@users.noreply.github.com>
2020-07-25 01:29:36 +0530

    Update Q9_copyMp3.py

--- Day2/Q9_copyMp3.py
+++ Day2/Q9_copyMp3.py
@@ -8,7 +8,7 @@ if sfname.endswith('.mp3'):
     dfname=input("Enter the destination file name: ")
     with open(sfname) as f:
         with open(dfname,'w') as f1:
-            shutil.copy(sfname,dfname)
+            shutil.copy(sfname,dfname)      #copies directly from one file to another
 else:
     print("Not an mp3 file")
         
